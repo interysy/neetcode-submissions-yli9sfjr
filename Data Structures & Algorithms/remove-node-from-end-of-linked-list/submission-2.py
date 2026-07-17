9a92084b4885060f1fed3036f9c4531d40977a1d
Michal Wozniak <86715742+interysy@users.noreply.github.com>
2026-07-17 15:29:53 +0100

    Add: remove-node-from-end-of-linked-list - submission-2

---
+++ Data Structures & Algorithms/remove-node-from-end-of-linked-list/submission-2.py
@@ -0,0 +1,44 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+
+        if not head: 
+            return None 
+
+        if not head.next and n == 1: 
+            return None
+        
+        prev_pointer_1 = None
+        pointer_1 = head 
+        pointer_2 = head
+        current_n = 0
+
+        while pointer_2:
+            print("Current pointer_1 " , pointer_1.val)
+            print("Current pointer_2 " , pointer_2.val)
+            print("Distance " , current_n)
+            pointer_2 = pointer_2.next
+
+            current_n += 1 
+
+            if current_n > n: 
+                current_n -= 1
+                prev_pointer_1 = pointer_1
+                pointer_1 = pointer_1.next
+
+        # print(current_n)
+        # print(pointer_1.val)
+        # pointer1 to remove
+        if prev_pointer_1 == None: 
+            # print(pointer_1.next.val)
+            return pointer_1.next
+
+        prev_pointer_1.next = pointer_1.next
+        pointer_1.next = None 
+        return head 
+             
